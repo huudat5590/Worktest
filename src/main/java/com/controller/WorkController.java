7852d93d13a00d3c8ac4da1d8dc0e3987d790978
Trần Hữu Đạt <dat.tranhuu@thegioididong.com>
2020-03-06 01:13:07 +0700

    commit finish code

--- src/main/java/com/controller/WorkController.java
+++ src/main/java/com/controller/WorkController.java
@@ -1,14 +1,20 @@
 package com.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.entities.Work;
@@ -32,6 +38,30 @@ private WorkRepsitory WorkRepsitory;
 public List <Work> findAll() {
 return WorkRepsitory.findAll();
 }
+
+
+//List all by sorting and pageable
+@RequestMapping(value="/findAllbypage",method = RequestMethod.GET,produces="application/json;charset=UTF-8")
+@ResponseBody
+public List <Work> findAllbypage(@RequestParam(defaultValue = "0") Integer pageNo, 
+        @RequestParam(defaultValue = "10") Integer pageSize,
+        @RequestParam(defaultValue = "workid") String sortBy) {
+	
+	Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+	 
+    Page<Work> pagedResult = WorkRepsitory.findAll(paging);
+	
+	if(pagedResult.hasContent()) {
+        return pagedResult.getContent();
+    } else {
+        return new ArrayList<Work>();
+    }
+	
+//return (List<Work>) WorkRepsitory.findAll(PageRequest.of(1, 5, Sort.by("sortBy").descending()));
+
+
+}
+
 
 
 // Update
